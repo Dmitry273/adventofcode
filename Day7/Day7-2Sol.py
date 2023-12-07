34cd88c8e6b377596b37867dee9e7a0c2cd9c20f
Catnee <ste01a01@gmail.com>
2023-12-07 15:09:54 +0400

    all solutions

--- Day7/Day7-2Sol.py
+++ Day7/Day7-2Sol.py
@@ -3,13 +3,19 @@ import pandas as pd
 data = pd.read_csv('Day7/day7mat.txt', sep= ' ', names= ['cards', 'bid'])
 
 def CardsValue(s) -> str:
+    ss = s.replace('J','')
     hands = [[1, 1, 1, 1, 1], [2, 1, 1, 1], [2, 2, 1], [3, 1, 1], [3, 2], [4, 1], [5]]
     cunts = []
-    for c in set(s):
-        cunts.append(s.count(c))
+    for c in set(ss):
+        cunts.append(ss.count(c))
     cunts.sort(reverse= True)
+    cunt = s.count('J')
+    if cunt == 5:
+        cunts = [5]
+    else:
+        cunts[0] += cunt
 
-    tops = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10}
+    tops = {'A': 14, 'K': 13, 'Q': 12, 'J': 1, 'T': 10}
     answ = chr(122 - hands.index(cunts))
     for A in s:
         B = tops[A] if A in tops else int(A)
