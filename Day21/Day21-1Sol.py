7a49ae73126fc8c02d9831b32727743b2c788c8f
Catnee <ste01a01@gmail.com>
2023-12-22 03:00:11 +0400

    now works with any number even with ugly layout

--- Day21/Day21-1Sol.py
+++ Day21/Day21-1Sol.py
@@ -1,13 +1,12 @@
 import time
 
 garden = []
-for n, line in enumerate(open('Day21/day21mat.txt', 'r')):
+for n, line in enumerate(open('Day21/day21mat-t.txt', 'r')):
     if 'S' in line: start = (n, line.index('S'))
     garden.append([1 if i!='#' else 0 for i in line[:-1]])
 
 H = len(garden)
 W = len(garden[0])
-#R, L, U, D = np.array([0,1]), np.array([0,-1]), np.array([-1,0]), np.array([1,0])
 R, L, U, D = (0,1), (0,-1), (-1,0), (1,0)
 
 def vadd(a: tuple, b: tuple) -> tuple:
@@ -15,14 +14,6 @@ def vadd(a: tuple, b: tuple) -> tuple:
 
 mem = {}
 def neighbors(coor):
-    # ind = (coor[0]%H, coor[1]%W)
-    # loc = (H*(coor[0]//H), W*(coor[1]//W))
-    # if ind not in mem: 
-    #     answ = list(filter(lambda x: garden[x[0]%H][x[1]%W] != '#', [vadd(ind, R), vadd(ind, L), vadd(ind, U), vadd(ind, D)]))
-    #     mem.update({ind: answ})
-    # return set(map(lambda x: vadd(loc, x),mem[ind]))
-
-    #return list(filter(lambda x: garden[x[0]%H][x[1]%W] != '#', [coor+ R, coor+ L, coor+ U, coor+ D]))
     return list(filter(lambda x: garden[x[0]%H][x[1]%W], [vadd(coor, R),vadd(coor, L),vadd(coor, U),vadd(coor, D)]))
 
 positions = [set([]),set([start])]
@@ -30,6 +21,8 @@ wb = [0,1]
 steps = 0
 k = 3
 m = 131*k+65
+m = 5000
+f = open('Day21/tryme.txt', 'a')
 t1 = time.time()
 while steps < m:
     color = steps%2
@@ -38,9 +31,10 @@ while steps < m:
     front = set().union(*map(neighbors, positions[not color]))
     front = front.difference(front&prev)
     wb[color] += len(front)
+    f.write(f'{wb[color]}\n')
     positions[color] = front
 t2 = time.time()
-#print(front)
+f.close()
 print(wb[color], f'{t2-t1:.2g} seconds, at {(t2-t1)/m:.2g} per step')
 k = 202300
 m = 131*k+65
--- Day21/Day21analyze-t.py
+++ Day21/Day21analyze-t.py
@@ -1,6 +1,16 @@
 black = [3784, 33680, 93366, 182842, 302108, 451164]
+data = [int(i[:-1]) for i in open('Day21/tryme.txt', 'r')]
+
+j =2
+print('.......................')
+print(f'At the {j} stage it is:')
+black = data[1000:][j::11]
+k = 0
 print(black)
 while not all([nu == black[0] for nu in black]):
+    k += 1
     black = [black[i]-black[i-1] for i in range(1, len(black))]
     print(black)
+print(f'with total of {k} power')
+print('.......................')
 #nope
